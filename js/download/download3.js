113d5757a1c96fe3cb856dcc9f67f6865c89e585
miguelafonsooo <ti212184@ensiguarda.pt>
2024-05-24 16:20:50 +0100

    iuauiaiuaiuauiaiui33

---
+++ js/download/download3.js
@@ -0,0 +1,17 @@
+
+        function changeImage(isHover) {
+        var imgButton = document.getElementById("imgButton");
+        if (isHover) {
+            imgButton.style.opacity = 0; // Torna a imagem invisível
+            setTimeout(function() {
+                imgButton.src = "../../images/download_branco.png"; // Troca a fonte da imagem
+                imgButton.style.opacity = 1; // Torna a imagem visível novamente
+            }, 300); // Atraso para garantir a transição
+        } else {
+            imgButton.style.opacity = 0; // Torna a imagem invisível
+            setTimeout(function() {
+                imgButton.src = "../../images/download_preto.png"; // Troca a fonte da imagem
+                imgButton.style.opacity = 1; // Torna a imagem visível novamente
+            }, 300); // Atraso para garantir a transição
+        }
+    }
