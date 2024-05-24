31724b7eae23c61812888aa2fcc0b964cc470b37
miguelafonsooo <ti212184@ensiguarda.pt>
2024-05-24 14:59:43 +0100

    uauiaiuauiuiaiui34

---
+++ header.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", function() {
+    var header = document.getElementById("main-header");
+    var logoImg = document.getElementById("logo-img");
+    if (window.scrollY > 0) {
+        header.classList.add("scroll-header");
+        logoImg.src = "images/geflogo.png"; // Trocar para o logotipo com fundo branco
+    } else {
+        header.classList.remove("scroll-header");
+        logoImg.src = "images/logobranco.png"; // Trocar de volta para o logotipo azul
+    }
+});
